b005930a65bdda758c3599e0aedbb4492d79fca8
Kevin Volanski <kvolanski@gmail.com>
2018-09-29 09:40:32 -0300

    Add files via upload

---
+++ dkrt-model/src/main/java/br/edu/fapi/dkrt/cliente/ClienteDTO.java
@@ -0,0 +1,50 @@
+package br.edu.fapi.dkrt.cliente;
+
+public class ClienteDTO {
+
+    private String nome;
+    private String nomeSocial;
+    private String cpf;
+    private String telefone;
+    private boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeSocial() {
+        return nomeSocial;
+    }
+
+    public void setNomeSocial(String nomeSocial) {
+        this.nomeSocial = nomeSocial;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
---
+++ dkrt-model/src/main/java/br/edu/fapi/dkrt/produto/ProdutoDTO.java
@@ -0,0 +1,41 @@
+package br.edu.fapi.dkrt.produto;
+
+public class ProdutoDTO {
+
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
