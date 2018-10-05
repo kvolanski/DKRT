2ba376b7ab269cb74406cc7fb4ef06633c36a808
Tiago <tiago.1301711@fapi-pinhais.edu.br>
2018-10-05 00:46:49 -0300

    Criação das Classes de modelo para Orçamento, Pedido e Venda, alteração da base usada no código para uma base de testes.

--- dkrt-master/dkrt-database/src/main/java/br/edu/fapi/dkrt/conexao/MySqlConnectionProvider.java
+++ dkrt-master/dkrt-database/src/main/java/br/edu/fapi/dkrt/conexao/MySqlConnectionProvider.java
@@ -5,8 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnectionProvider {
+//      Base original
+//    public static String url = "jdbc:mysql://localhost:3306/dkrt_erp";
 
-    public static String url = "jdbc:mysql://localhost:3306/dkrt_erp";
+//      Base teste
+    public static String url = "jdbc:mysql://localhost:3306/dkrt_erp_teste";
     public static String usuario = "root";
     public static String senha = "";
 
---
+++ dkrt-master/dkrt-model/src/main/java/br/edu/fapi/dkrt/orcamento/OrcamentoDTO.java
@@ -0,0 +1,69 @@
+package br.edu.fapi.dkrt.orcamento;
+
+import java.util.Date;
+
+public class OrcamentoDTO {
+    private int id;
+    private float valorTotal;
+    private String status;
+    private int desconto;
+    private Date dataGeracao;
+    private Date dataExpiracao;
+    private int pedidoId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+
+    public Date getDataGeracao() {
+        return dataGeracao;
+    }
+
+    public void setDataGeracao(Date dataGeracao) {
+        this.dataGeracao = dataGeracao;
+    }
+
+    public Date getDataExpiracao() {
+        return dataExpiracao;
+    }
+
+    public void setDataExpiracao(Date dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(int pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+}
